cd6549236a2697bf959bac04fa2f2ccaf22426d1
ReedGit <350099287@qq.com>
2015-12-30 15:59:33 +0800

    init Project

---
+++ src/com/bysj/trip/entity/Content.java
@@ -0,0 +1,99 @@
+package com.bysj.trip.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "content")
+public class Content {
+
+	private long contentId;
+	private String article;
+	private String location;
+	private Date time;
+	private long travelId;
+	private String coordinate;
+	private int day;
+
+	@Id
+	@GenericGenerator(name = "generator", strategy = "native")
+	@GeneratedValue(generator = "generator")
+	@Column(name = "contentid")
+	public long getContentId() {
+		return contentId;
+	}
+
+	public void setContentId(long contentId) {
+		this.contentId = contentId;
+	}
+
+	@Column(name = "article")
+	public String getArticle() {
+		return article;
+	}
+
+	public void setArticle(String article) {
+		this.article = article;
+	}
+
+	@Column(name = "location")
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	@Column(name = "time")
+	public Date getTime() {
+		return time;
+	}
+
+	public void setTime(Date time) {
+		this.time = time;
+	}
+
+	@Column(name = "travelid")
+	public long getTravelId() {
+		return travelId;
+	}
+
+	public void setTravelId(long travelId) {
+		this.travelId = travelId;
+	}
+
+	@Column(name = "coordinate")
+	public String getCoordinate() {
+		return coordinate;
+	}
+
+	public void setCoordinate(String coordinate) {
+		this.coordinate = coordinate;
+	}
+
+	@Column(name = "day")
+	public int getDay() {
+		return day;
+	}
+
+	public void setDay(int day) {
+		this.day = day;
+	}
+
+	@Override
+	public String toString() {
+		return "Content [contentId=" + contentId + ", article=" + article
+				+ ", location=" + location + ", time=" + time + ", travelId="
+				+ travelId + ", coordinate=" + coordinate + ", day=" + day
+				+ "]";
+	}
+
+}
---
+++ src/com/bysj/trip/entity/ContentImage.java
@@ -0,0 +1,55 @@
+package com.bysj.trip.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "contentimage")
+public class ContentImage {
+
+	private long imageId;
+	private long contentId;
+	private String imageUrl;
+
+	@Id
+	@GenericGenerator(name = "generator", strategy = "native")
+	@GeneratedValue(generator = "generator")
+	@Column(name = "imageid")
+	public long getImageId() {
+		return imageId;
+	}
+
+	public void setImageId(long imageId) {
+		this.imageId = imageId;
+	}
+
+	@Column(name = "contentid")
+	public long getContentId() {
+		return contentId;
+	}
+
+	public void setContentId(long contentId) {
+		this.contentId = contentId;
+	}
+
+	@Column(name = "imageurl")
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	@Override
+	public String toString() {
+		return "ContentImage [imageId=" + imageId + ", contentId=" + contentId
+				+ ", imageUrl=" + imageUrl + "]";
+	}
+
+}
